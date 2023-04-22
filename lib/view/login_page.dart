cf9e057aa1c2efa87bd0a7bb7704d5ddd8fec702
jonsonkurt <55541726+jonsonkurt@users.noreply.github.com>
2023-04-22 21:27:11 +0800

    Added password obscure text

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -40,174 +40,173 @@ class _LoginPageState extends State<LoginPage> {
               color: Colors.brown.withOpacity(0.6),
               borderRadius: BorderRadius.circular(20),
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(height: 15),
-                // const CircleAvatar(
-                //   radius: 50,
-                //   backgroundImage: AssetImage("assets/images/snap_logo.png"),
-                // ),
-                const SizedBox(height: 10),
-                const Text(
-                  "SIGN IN",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 15),
+                  const SizedBox(height: 10),
+                  const Text(
+                    "SIGN IN",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 30),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const SignUpPage()),
-                    );
-                  },
-                  child: const Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Not registered yet? Sign Up",
-                      style: TextStyle(
-                        color: Colors.white,
+                  const SizedBox(height: 30),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SignUpPage()),
+                      );
+                    },
+                    child: const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Not registered yet? Sign Up",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: emailController,
-                          style: const TextStyle(color: Colors.white),
-                          decoration: const InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
+                  const SizedBox(height: 20),
+                  Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: emailController,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter your email',
+                              labelText: 'Email',
+                              labelStyle: TextStyle(color: Colors.white),
+                              hintStyle: TextStyle(color: Colors.green),
                             ),
-                            border: OutlineInputBorder(),
-                            hintText: 'Enter your email',
-                            labelText: 'Email',
-                            labelStyle: TextStyle(color: Colors.white),
-                            hintStyle: TextStyle(color: Colors.green),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              return null; // Return null if there is no error
+                            },
                           ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your email';
-                            }
-                            return null; // Return null if there is no error
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
-                          controller: passwordController,
-                          style: const TextStyle(color: Colors.white),
-                          decoration: const InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
+                          const SizedBox(height: 20),
+                          TextFormField(
+                            obscureText: true,
+                            controller: passwordController,
+                            style: const TextStyle(color: Colors.white),
+                            decoration: const InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              border: OutlineInputBorder(),
+                              hintText: 'Enter your password',
+                              labelText: 'Password',
+                              labelStyle: TextStyle(color: Colors.white),
+                              hintStyle: TextStyle(color: Colors.green),
                             ),
-                            border: OutlineInputBorder(),
-                            hintText: 'Enter your password',
-                            labelText: 'Password',
-                            labelStyle: TextStyle(color: Colors.white),
-                            hintStyle: TextStyle(color: Colors.green),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              return null; // Return null if there is no error
+                            },
                           ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your password';
-                            }
-                            return null; // Return null if there is no error
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const ForgotPasswordPage()),
-                            );
-                          },
-                          child: const Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              "Forgot Password?",
+                          const SizedBox(height: 20),
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const ForgotPasswordPage()),
+                              );
+                            },
+                            child: const Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                "Forgot Password?",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 25),
+                          OutlinedButton(
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                String email = emailController.text;
+                                String password = passwordController.text;
+                                bool isUserExist = await DatabaseHelper.instance
+                                    .isUserExist(email, password);
+
+                                if (isUserExist) {
+                                  final List<Map<String, dynamic>> result =
+                                      await DatabaseHelper.instance.getUsers();
+                                  final Map<String, dynamic> user = result
+                                      .firstWhere((u) => u['email'] == email);
+                                  int userId = user['id'];
+                                  DatabaseHelper.loggedInUserId = userId;
+                                  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (context, animation,
+                                              secondaryAnimation) =>
+                                          const MyScreen(),
+                                      transitionsBuilder: (context, animation,
+                                          secondaryAnimation, child) {
+                                        var begin = const Offset(0.0, 1.0);
+                                        var end = Offset.zero;
+                                        var curve = Curves.ease;
+
+                                        var tween = Tween(
+                                                begin: begin, end: end)
+                                            .chain(CurveTween(curve: curve));
+
+                                        return SlideTransition(
+                                          position: animation.drive(tween),
+                                          child: child,
+                                        );
+                                      },
+                                    ),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content:
+                                            Text('Invalid email or password')),
+                                  );
+                                }
+                              }
+                            },
+                            style: OutlinedButton.styleFrom(
+                              shape: const StadiumBorder(),
+                              side: const BorderSide(
+                                  color: Colors.white, width: 2),
+                            ),
+                            child: const Text(
+                              "    SIGN IN    ",
                               style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
                                 color: Colors.white,
                               ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 25),
-                        OutlinedButton(
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              String email = emailController.text;
-                              String password = passwordController.text;
-                              bool isUserExist = await DatabaseHelper.instance
-                                  .isUserExist(email, password);
-
-                              if (isUserExist) {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => const MyScreen()),
-                                );
-                              } else {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content:
-                                          Text('Invalid email or password')),
-                                );
-                              }
-                            }
-                          },
-                          // onPressed: () {
-                          //   Navigator.push(
-                          //     context,
-                          //     PageRouteBuilder(
-                          //       pageBuilder:
-                          //           (context, animation, secondaryAnimation) =>
-                          //               const MyScreen(),
-                          //       transitionsBuilder: (context, animation,
-                          //           secondaryAnimation, child) {
-                          //         var begin = const Offset(0.0, 1.0);
-                          //         var end = Offset.zero;
-                          //         var curve = Curves.ease;
-
-                          //         var tween = Tween(begin: begin, end: end)
-                          //             .chain(CurveTween(curve: curve));
-
-                          //         return SlideTransition(
-                          //           position: animation.drive(tween),
-                          //           child: child,
-                          //         );
-                          //       },
-                          //     ),
-                          //   );
-                          // },
-                          style: OutlinedButton.styleFrom(
-                            shape: const StadiumBorder(),
-                            side:
-                                const BorderSide(color: Colors.white, width: 2),
-                          ),
-                          child: const Text(
-                            "    SIGN IN    ",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    )),
-                const SizedBox(height: 15),
-              ],
+                        ],
+                      )),
+                  const SizedBox(height: 15),
+                ],
+              ),
             ),
           ),
         ),
