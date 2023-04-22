d3e5caa978a3de15f51ea6b72f31eea5ca09ceaa
jonsonkurt <55541726+jonsonkurt@users.noreply.github.com>
2023-04-22 19:03:41 +0800

    Added database file

---
+++ lib/model/database_helper.dart
@@ -0,0 +1,70 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('snap.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        email TEXT,
+        password TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertUser({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    final db = await database;
+
+    return await db.insert(
+      'users',
+      {
+        'name': name,
+        'email': email,
+        'password': password,
+      },
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> getUsers() async {
+    final db = await database;
+
+    return await db.query('users');
+  }
+
+  Future<bool> isUserExist(String email, String password) async {
+    final db = await database;
+
+    final result = await db.query(
+      'users',
+      where: 'email = ? AND password = ?',
+      whereArgs: [email, password],
+    );
+
+    return result.isNotEmpty;
+  }
+}
