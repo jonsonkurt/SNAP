a423b88af4ca1518d4449328c00972b61a0f9bf8
Jairo <jairojoaquinafable@gmail.com>
2023-06-07 17:24:15 +0800

    Added recommendation feature (manual readings)

--- lib/view/record_page.dart
+++ lib/view/record_page.dart
@@ -21,41 +21,81 @@ class _RecordPageState extends State<RecordPage> {
   bool _scanningComplete = false;
 
   Future<void> _scanSoilQualities() async {
-    // Get the current date and time
-    DateTime now = DateTime.now();
+  // Get the current date and time
+  DateTime now = DateTime.now();
 
-    // Format the date and time as strings
-    String formattedDate = DateFormat('dd-MM-yyyy').format(now);
-    String formattedTime = DateFormat('HH:mm').format(now);
+  // Format the date and time as strings
+  String formattedDate = DateFormat('dd-MM-yyyy').format(now);
+  String formattedTime = DateFormat('HH:mm').format(now);
 
-    // Implement soil scanning logic here
-    // Once the soil scanning is complete, update the state with the results
-    setState(() {
-      _date = formattedDate;
-      _time = formattedTime;
-      _phValue = '6.5';
-      _npk = '14, 14, 14';
-      _humidity = '95%';
-      _temperature = '28';
-      _recommendedCrop = 'Grapes and Ginger';
-      _scanningComplete =
-          true; // Set the variable to true when scanning is complete
-    });
-    DatabaseHelper databaseHelper = DatabaseHelper.instance;
-    await databaseHelper.insertRecording(
-      userId: '${DatabaseHelper.loggedInUserId}',
-      date: _date!,
-      time: _time!,
-      ph: _phValue!,
-      n: _npk!.split(',')[0],
-      p: _npk!.split(',')[1],
-      k: _npk!.split(',')[2],
-      humidity: _humidity!,
-      temperature: '${_temperature!}°C',
-      plant: _recommendedCrop!.split(' and ')[0],
-      crop: _recommendedCrop!.split(' and ')[1],
-    );
+  // Implement soil scanning logic here
+  // Once the soil scanning is complete, update the state with the results
+  setState(() {
+    _date = formattedDate;
+    _time = formattedTime;
+    _phValue = '8';
+    _npk = '6, 8, 9';
+    _humidity = '70%';
+    _temperature = '28';
+    _scanningComplete = true; // Set the variable to true when scanning is complete
+  });
+
+  // Perform recommendation based on NPK and pH values
+  List<dynamic> recommendedPlants = [];
+  List<dynamic> suitablePlants = [];
+
+  for (List<dynamic> plantData in plantDatabase) {
+    double minPh = plantData[1];
+    double maxPh = plantData[2];
+    int minN = plantData[3];
+    int minP = plantData[4];
+    int minK = plantData[5];
+
+    List<String> npkValues = _npk!.split(', ');
+    int scannedN = int.parse(npkValues[0]);
+    int scannedP = int.parse(npkValues[1]);
+    int scannedK = int.parse(npkValues[2]);
+
+    if (_phValue != null &&
+        double.parse(_phValue!) >= minPh &&
+        double.parse(_phValue!) <= maxPh &&
+        scannedN >= minN &&
+        scannedP >= minP &&
+        scannedK >= minK) {
+      recommendedPlants.add(plantData[0]);
+    }
+
+    // Check if the hard-coded pH value falls within the suitable pH range
+    if (_phValue != null &&
+        double.parse(_phValue!) >= minPh &&
+        double.parse(_phValue!) <= maxPh) {
+      suitablePlants.add(plantData[0]);
+    }
   }
+
+  if (recommendedPlants.isNotEmpty) {
+    _recommendedCrop = '${recommendedPlants[0]} and ${suitablePlants[1]}'; // Display the first two suitable plants
+  } else if (suitablePlants.isNotEmpty) {
+    _recommendedCrop = suitablePlants[0]; // Get the first suitable plant
+  } else {
+    _recommendedCrop = 'No recommended crops found';
+  }
+
+  DatabaseHelper databaseHelper = DatabaseHelper.instance;
+  await databaseHelper.insertRecording(
+    userId: '${DatabaseHelper.loggedInUserId}',
+    date: _date!,
+    time: _time!,
+    ph: _phValue!,
+    n: _npk!.split(',')[0],
+    p: _npk!.split(',')[1],
+    k: _npk!.split(',')[2],
+    humidity: _humidity!,
+    temperature: '${_temperature!}°C',
+    plant: _recommendedCrop!.split(' and ')[0],
+    crop: suitablePlants.isNotEmpty ? suitablePlants[1] : '', // Get the first plant from suitablePlants list
+  );
+}
 
   @override
   Widget build(BuildContext context) {
